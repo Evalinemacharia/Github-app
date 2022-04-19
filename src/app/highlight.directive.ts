3623566add4ff30dfc0eba91839a0aa37a896cce
Evaline Wangu <evalinemacharia99@gmail.com>
2022-04-20 00:13:51 +0300

    Add change on the highlight

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -11,7 +11,7 @@ export class HighlightDirective {
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.highlight("brown");
+    this.highlight("black");
   }
   private highlight(color: string) {
     this.el.nativeElement.style.backgroundColor = color;
